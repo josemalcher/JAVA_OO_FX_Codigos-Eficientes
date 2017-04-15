c8a575f5c9a6b2d4c18473327dd63b00be053b8f
José Malcher Junior <contato@josemalcher.net>
2017-04-15 11:31:26 -0300

    Java - Aula 3 - Usando a classe JOptionPane

---
+++ Aulas-JAVA-OO-01/src/javaooaula03/Animal.java
@@ -0,0 +1,46 @@
+package javaooaula03;
+
+import javaooaula02.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author josemalcher
+ * 
+ * Java - Aula 3 - Usando a classe JOptionPane
+ * https://www.youtube.com/watch?v=pretB2Uny14
+ * 
+ */
+public class Animal {
+    private String nome;
+    private int idade;
+    private float altura;
+    private String especie;
+
+    public Animal() {
+    }
+    
+    public Animal(String nome, int idade, float altura, String especie) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+        this.especie = especie;
+    }
+    
+    public void MostrarCaracteristivas(){
+        nome = JOptionPane.showInputDialog("Digite o nome do animal");
+        idade = Integer.parseInt(JOptionPane.showInputDialog("DIgite a idade do animal"));
+        altura = Float.parseFloat(JOptionPane.showInputDialog("Digite a altura do animal"));
+        especie = JOptionPane.showInputDialog("Digite a espécie do animal");
+        System.out.println("javaooaula02.Animal.MostrarCaracteristivas()");
+        System.out.println("O nome do animal é "+ this.nome);
+        System.out.println("A idade do animal é "+ this.idade);
+        System.out.println("A altura do animal é "+ this.altura);
+        System.out.println("A especie do animal é "+ this.especie);
+    }
+    
+    public void FazerBarulho(){
+        System.out.println("O animal fez barulho");
+    }
+    
+}
